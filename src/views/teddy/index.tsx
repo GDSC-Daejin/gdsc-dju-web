e421165eced42320031116d50d5fe007babe3ac5
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-05-11 17:25:57 +0900

    테디 홈페이지 Top5 페이지 weekly 페이지

--- src/views/teddy/index.tsx
+++ src/views/teddy/index.tsx
@@ -1,1 +1,3 @@
 export { TeddyPage } from './ui/teddy-page';
+export { TeddyTop5 } from './ui/teddy-top5';
+export { TeddyWeekly } from './ui/teddy-weekly';
---
+++ src/views/teddy/ui/teddy-top5.tsx
@@ -0,0 +1,9 @@
+import { TeddyTop5Home } from '@/widgets/teddyTop5';
+
+export const TeddyTop5 = () => {
+  return (
+    <>
+      <TeddyTop5Home />
+    </>
+  );
+};

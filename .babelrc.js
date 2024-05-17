4a561d224ad058851f456a975d65b2001ae769ef
dntjd1097 <dntjd1097@gmail.com>
2024-05-17 19:15:10 +0900

    hotfix : babel

--- .babelrc.js
+++ .babelrc.js
@@ -10,7 +10,7 @@ module.exports = {
         genConditionalClasses: true,
         treeshakeCompensation: true,
         aliases: {
-          '@/*': [path.join(__dirname, '*')],
+          '@': './src',
         },
         unstable_moduleResolution: {
           type: 'commonJS',
